8cb3cc176b6e91f704c0c09187b48c46785b9813
Nicholas Waterman <63140071+aquaman48@users.noreply.github.com>
2021-02-07 11:34:40 -0800

    Echo statement in PHP

---
+++ Sandbox/echo_example.php
@@ -0,0 +1,10 @@
+<?php
+	echo "<p>Explore <strong>Africa </strong>, <br />";
+	echo "<strong> South America</strong>, <br />";
+	echo " and <strong>Australia</strong>!</p>";
+?>
+<?php
+	$Class = exciting;
+	echo "<p> Class is very $Class</p>"; 
+	//echo $Class;
+?>
