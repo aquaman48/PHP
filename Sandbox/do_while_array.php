ae3e8e3f161e45b1e3083f4ac13dd48f5581c715
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-02-09 15:53:11 -0800

    Do While Loop/Array
    Simple example of a Do While Loop with the use of an Array.

---
+++ Sandbox/do_while_array.php
@@ -0,0 +1,8 @@
+<?php
+$DaysOfWeek = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"); 
+$Count = 0; 
+do { echo $DaysOfWeek[$Count], "<br />"; 
+++$Count; } 
+while ($Count < 7);
+
+?>
