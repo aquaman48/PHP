7e73d08fda2689d1cf321b95ef8450f1aaf7b81c
Nicholas Waterman <63140071+aquaman48@users.noreply.github.com>
2021-02-08 17:30:30 -0800

    Function Example

---
+++ Sandbox/FunctionExample.php
@@ -0,0 +1,9 @@
+<?php
+function displayCompanyName($Company1, $Company2, $Company3) 
+{ 
+echo "<p>$Company1 </p>"; 
+echo "<p>$Company2 </p>"; 
+echo "<p>$Company3 </p>"; 
+}
+displayCompanyName("Tesla", "Apple", "Google");
+?>
