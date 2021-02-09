511703990abf4d2d711649eb2a50220ca9bb1eec
Nicholas Waterman <63140071+aquaman48@users.noreply.github.com>
2021-02-08 17:28:27 -0800

    Functions

---
+++ Sandbox/FuncReturn.php
@@ -0,0 +1,24 @@
+<?php
+function averageNumbers($a, $b, $c) 
+{ 
+$SumOfNumbers = $a + $b + $c; 
+$Result = $SumOfNumbers / 3; 
+return $Result; 
+}
+$Answer = averageNumbers(1,2,3);
+echo "<p> The answer is ",$Answer, "</p>";
+?>
+
+<?PHP
+
+ 
+
+ function avergeNumbers($a, $b, $c) {
+    $SumofNumbers = $a + $b + $c;
+    $Result = $SumofNumbers / 3;
+    return $Result;
+ }
+   
+    $Answer = averageNumbers(1,2,3);
+    echo "<p> The answer is ", $Answer, "</p>";
+?>
