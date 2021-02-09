be010334675b5dc05463f65d0314ddd9f43dab06
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-02-08 21:16:03 -0800

    If Example
    Uploading via GitHub Desktop app after cloning repostitory.

---
+++ Sandbox/IfState.php
@@ -0,0 +1,10 @@
+<?php
+$ExampleVar = 5; 
+if ($ExampleVar == 5) 
+{
+echo "<p> The condition evaluates to true.</p>"; 
+echo "<p>$ExampleVar is equal to ", $ExampleVar, ".</p>"; 
+echo "<p> Each of these lines will be displayed.</p>"; 
+} 
+echo "<p>This statement always executes after the 'if' statement. </p>";
+?>
