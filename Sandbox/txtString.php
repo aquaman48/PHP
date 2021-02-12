d8fd2b1999dcf2f2b87217c0a70c178ca61026c2
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-02-12 15:24:18 -0800

    String Variables

---
+++ Sandbox/txtString.php
@@ -0,0 +1,8 @@
+<?php
+	echo "<p>PHP literal text string</p>";
+	$StringVariable = "<p>PHP string variable</p>";
+	echo $StringVariable;
+
+	$Speaker = "Julius Caesar";
+	echo "<p>\"Et tu, Brute!\" exclaimed $Speaker.</p>";
+?>
