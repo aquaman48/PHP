d351b8c0736e5ac6c933d1e928f3dc78a65abc1c
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-02-13 10:25:38 -0800

    Simple BlackJack PHP

---
+++ Sandbox/BlackJack.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html>
+<title>Comparison Examples </title>
+
+<?php
+$BlackjackPlayer1 = 20; 
+($BlackjackPlayer1 <= 21) ? $Result = "Player 1 is still in the game." : $Result = "Player 1 is out of the action."; 
+echo "<p>", $Result, "</p> ";
+
+?>
