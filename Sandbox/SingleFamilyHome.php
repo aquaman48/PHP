a653eeef421db64c5d6f0f4d78c6328ca23c4283
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-02-19 17:51:32 -0800

    Returning a value.

---
+++ Sandbox/SingleFamilyHome.php
@@ -0,0 +1,16 @@
+<!DOCTYPEhtmlpublic"-//W3C//DTD HTML 4.01 strictl//EN"
+http://www/w3.org/TR/html4/strict.dtd">
+<html>
+<title>Single Family Home</title>
+<body>
+<?php
+	$SingleFamilyHome = 399500;
+$SingleFamilyHome_Display =
+ number_format($SingleFamilyHome, 2);
+echo "<p>The current median price of a single family
+ home in Pleasanton, CA is
+ $$SingleFamilyHome_Display.</p>";
+ ?>
+ </body>
+ 
+ </html>
